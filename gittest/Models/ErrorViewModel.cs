fc1c639c57ea2f89fe32a3dceafde4ffdc81300e
BaNi <BaNi@DESKTOP-B3HBU7N>
2022-07-24 01:04:02 -0700

    Add project files.

---
+++ gittest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gittest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
